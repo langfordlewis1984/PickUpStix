caa14ff756f58e0b55a90cc17a5fb36908ee7e50
Nick Lewis <nick@TechEd-0006.techeducators.co.uk>
2023-06-08 16:46:37 +0100

    PUSsite Plus JS

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # PickUpStix
+
+Starting all over again bec ause I got lost in the weeds wth HTML Seantics Demo
+
+Previous readme said:
+
+HTML semantics demonstration
+
+Create a wireframe
+make sure its low fidelity
+use pen and paper (or figma if you feel fancy)
+
+create new repo
+clone onto local git
+open in vs code
+create index file
+
+Write semantic HTML
+
+Write CSS
+
+Write JavaScript
---
+++ app.js
@@ -0,0 +1,18 @@
+let userName = prompt ("Greetings fellow Picker! How should one refer to you?");
+console.log(userName);
+
+const welcomeMsg = alert ("Welcome " + userName + "! Let's get PICKIN!");
+
+
+
+if (userName === "Rich") {
+    console.log("I liked your firey looking trainers today");
+} else if (userName === "Sam") {
+    console.log("Was nice to meet ya, thanks for your help and safe journey to Liverpool!")
+}else if (userName === "Chris") {
+    console.log("What do you bench, bro?")
+}else {
+    console.log('there aint nobody else here, so you must be looking at me')
+}
+
+console.log("PS my shiz is best viewd at a width of 880px!")
